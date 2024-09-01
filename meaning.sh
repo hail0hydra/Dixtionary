3e26f9032ad1f70d078be42baba6ca573dcbb9e0
hail0hydra <hail@gmail.com>
2024-09-01 22:04:33 +0530

    slight change

--- meaning.sh
+++ meaning.sh
@@ -17,7 +17,6 @@ fi
 aud=`curl --silent https://api.dictionaryapi.dev/api/v2/entries/en/$1 | jq .[].'phonetics'.[].'audio' | grep us`
 c=${aud//\"/} # removing "" from link, dunno why mpv bugs with it :(
 c=$(echo "$c" | head -n 1 | sed 's/.*\(https?:\/\/[^ ]*\).*/\1/') # if we get multiple links, we just want to play only one ;)
-echo $c
 
 if [ ! -z "$c" ]; then
     mpv  $c 1>/dev/null
